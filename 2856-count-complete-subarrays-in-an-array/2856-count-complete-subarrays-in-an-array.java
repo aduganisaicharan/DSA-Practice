15cd7f078e5c43857c669967149e20a652fcf444
A Sai charan <amandusaicharan13@gmail.com>
2025-04-25 02:15:47 +0530

    Time: 7 ms (89.79%), Space: 45.1 MB (46.32%) - LeetHub

--- 2856-count-complete-subarrays-in-an-array/2856-count-complete-subarrays-in-an-array.java
+++ 2856-count-complete-subarrays-in-an-array/2856-count-complete-subarrays-in-an-array.java
@@ -1,31 +1,24 @@
 class Solution {
     public int countCompleteSubarrays(int[] nums) {
-        int cnt = 0;
         HashSet<Integer> set = new HashSet<>();
-        for(int i=0;i<nums.length;i++){
-            set.add(nums[i]);
-        }
+        for(int i:nums)set.add(i);
         int i=0;
         int j=0;
-        while(i<nums.length){
-            HashMap<Integer, Integer> hash = new HashMap<>();
-            if(hash.containsKey(nums[i]) && hash.get(nums[i])>1){
-                hash.put(nums[i], hash.get(nums[i])-1);
-            }
-            else if(hash.containsKey(nums[i]) && hash.get(nums[i]) == 1){
-                hash.remove(nums[i]);
-            }
-            j=i;
-            while(j<nums.length){
-                hash.put(nums[j], hash.getOrDefault(nums[j], 0)+1);
-                if(hash.size() == set.size()){
-                    cnt++;
-                    cnt = cnt+(nums.length - (j+1));
-                    break;
+        int cnt = 0;
+        HashMap<Integer, Integer> hash = new HashMap<>();
+        while(j<nums.length && i<nums.length){
+            hash.put(nums[j], hash.getOrDefault(nums[j], 0)+1);
+            while(hash.size() == set.size()){
+                cnt+= nums.length - j;
+                if(hash.get(nums[i])>1){
+                    hash.put(nums[i], hash.get(nums[i])-1);
                 }
-                j++;
+                else if(hash.get(nums[i]) == 1){
+                    hash.remove(nums[i]);
+                }
+                i++;
             }
-            i++;
+            j++;
         }
         return cnt;
     }
